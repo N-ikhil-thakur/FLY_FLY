c7279a4cbca8be7f8a717409d73ef28658f79186
Nikhil Thakur <52308151+N-ikhil-thakur@users.noreply.github.com>
2019-08-30 18:39:31 +0545

    Update space.js

--- space.js
+++ space.js
@@ -1,19 +1,19 @@
 var x=10;
 var y=10;
 function setup(){
-createCanvas(800,800);
-background(220);
-frameRate(200);
+// createCanvas(800,800);
+// background(220);
+// frameRate(200);
 
 }
 
-function draw(){
+// function draw(){
 
-x++;
-    rect(mouseX,mouseY,30,30);
+// x++;
+//     rect(mouseX,mouseY,30,30);
 
 
-}
+// }
 
 
 function keyPressed() {
