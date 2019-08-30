8a5269fe97ced15a40cbdc88e69f8df35e8a6bee
Nikhil Thakur <nikhilthakur281@gmail.com>
2019-08-30 18:35:20 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+var x=0;
+var y=0;
+var z=0;
+var ym=0;
+var interval;
+function run(){
+    // x=z;
+    ym++;
+x+=2;
+document.querySelector(".box").style.transform=`translatey(${x}px)`;
+if(x==800){
+    x=0;
+}
+
+if(x==-40){
+    x=800;
+}
+
+
+
+}
+
+interval=setInterval(run,10);  
+
+function up(){
+    // y++;
+    z=x-55;
+    x=z;
+    document.querySelector(".box").style.transform=`translatey(${z}px)`;
+    // clearInterval(interval);
+}
---
+++ space.js
@@ -0,0 +1,25 @@
+var x=10;
+var y=10;
+function setup(){
+createCanvas(800,800);
+background(220);
+frameRate(200);
+
+}
+
+function draw(){
+
+x++;
+    rect(mouseX,mouseY,30,30);
+
+
+}
+
+
+function keyPressed() {
+    if (keyCode === 32) {
+    //   document.querySelector("#board").style.background="red";
+    up();
+    }
+    return false;
+  }
